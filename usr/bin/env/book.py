59108aaf5e3096e2345062ce3513e3a9c0f63d62
Lorenzo <lorenzo.mendes@edu.devinci.fr>
2022-03-28 17:21:57 +0200

    Exercice 2

---
+++ usr/bin/env/book.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self,name):
+        self.name=name
+
+
+class Order:
+    def __init__(self,quantity,price):
+        self.quantity=quantity
+        self.price=price
+
+    def insert_buy(self):
+        __init__(self,quantity,price)
+
+    def insert_sell(self):
+        __init__(self,quantity,price)
+
+
